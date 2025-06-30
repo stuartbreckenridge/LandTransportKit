a686d1d1007960448ed1334c1e4d26104c153bf7
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2025-06-30 13:57:53 +0800

    Initial Commit

---
+++ LandTransportKit/LandTransportKit.swift
@@ -0,0 +1,9 @@
+//
+//  LandTransportKit.swift
+//  LandTransportKit
+//
+//  Created by Stuart Breckenridge on 30/06/2025.
+//
+
+import Foundation
+
---
+++ LandTransportKitTests/LandTransportKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandTransportKitTests.swift
+//  LandTransportKitTests
+//
+//  Created by Stuart Breckenridge on 30/06/2025.
+//
+
+import Testing
+@testable import LandTransportKit
+
+struct LandTransportKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
