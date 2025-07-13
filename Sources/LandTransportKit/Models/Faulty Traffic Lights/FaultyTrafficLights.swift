fb6cfd0e658b0f1540c66a00021334172b633f74
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2025-07-13 11:37:19 +0800

    💡 Updated commentary

--- Sources/LandTransportKit/Models/Faulty Traffic Lights/FaultyTrafficLights.swift
+++ Sources/LandTransportKit/Models/Faulty Traffic Lights/FaultyTrafficLights.swift
@@ -21,7 +21,7 @@ internal struct FaultyTrafficLights: Codable, Sendable {
 /// - Properties:
 ///   - AlarmID: The unique identifier for the specific alarm or event.
 ///   - NodeID: The identifier of the node (traffic light) affected.
-///   - Type: The type or category of the fault (e.g., "Fault", "Scheduled").
+///   - Type: The type or category of the fault (e.g., "4" — Blackout, "13" — Flashing Yellow).
 ///   - StartDate: The date and time when the fault or maintenance started, as a string in "YYYY-MM-DD HH:MM:SS.ms" format (Asia/Singapore time zone).
 ///   - EndDate: The date and time when the fault or maintenance ended (for maintenance), as a string in "YYYY-MM-DD HH:MM:SS.ms" format (Asia/Singapore time zone). If empty, indicates an ongoing incident.
 ///   - Message: A human-readable description of the fault or scheduled maintenance.
@@ -30,6 +30,8 @@ internal struct FaultyTrafficLights: Codable, Sendable {
 ///   - iso8601StartDate: The parsed start date as a `Date` object, or `nil` if unavailable or invalid.
 ///   - iso8601EndDate: The parsed end date as a `Date` object, or `nil` if unavailable or invalid.
 ///   - isScheduledMaintenance: Returns `true` if the incident is a scheduled maintenance (based on whether `EndDate` is not empty), or `false` otherwise.
+///
+///  - Note: Not all `Type`s of incident are documented. The API may return "2" for `Type` but the `Message` returned will be `(13/7)11:03 Black Out at Woodlands Avenue 6/Woodlands Drive 65 Junction.`
 public struct FaultyTrafficLight: Codable, Sendable {
     let AlarmID: String
     let NodeID: String
